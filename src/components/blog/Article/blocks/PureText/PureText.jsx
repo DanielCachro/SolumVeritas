cf9d5308cbc2e4ad23c3198eb2e7356af2138bbb
DanielCachro <danielcachro@gmail.com>
2025-04-08 16:25:00 +0200

    feat: integrate react-markdown for rendering text blocks
    - Added react-markdown dependency to package.json.
    - Updated `PureText` & `TextBlock` component to use ReactMarkdown for rendering text.
    - Added styles for text formatting in `PureText` & `TextBlock` module CSS.
    - Created robots.txt to manage web crawler access.

--- src/components/blog/Article/blocks/PureText/PureText.jsx
+++ src/components/blog/Article/blocks/PureText/PureText.jsx
@@ -1,3 +1,10 @@
-export default function PureText({text, ...props}) {
-	return <p {...props}>{text}</p>
+import ReactMarkdown from 'react-markdown'
+import classes from './PureText.module.css'
+
+export default function PureText({text, className = '', ...props}) {
+	return (
+		<div className={`${classes.textBlock} ${className}`.trim()} {...props}>
+			<ReactMarkdown>{text}</ReactMarkdown>
+		</div>
+	)
 }
--- src/components/blog/Article/blocks/TextBlock/TextBlock.jsx
+++ src/components/blog/Article/blocks/TextBlock/TextBlock.jsx
@@ -1,10 +1,11 @@
 import classes from './TextBlock.module.css'
+import ReactMarkdown from 'react-markdown'
 
 export default function TextBlock({title, text, className = '', ...props}) {
 	return (
 		<div className={`${classes.textBlock} ${className}`.trim()} {...props}>
 			<h3 className={classes.textBlock__title}>{title}</h3>
-			<p>{text}</p>
+			<ReactMarkdown>{text}</ReactMarkdown>
 		</div>
 	)
 }
